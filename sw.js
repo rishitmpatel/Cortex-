6127da32cabdf1905227f2527da3fb097109a329
rishitmpatel <rishitmpatel2312@gmail.com>
2026-09-23 09:46:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Cortex service worker */
 
-const CACHE_NAME = 'cortex-v3';
+const CACHE_NAME = 'cortex-v4';
 
 const APP_SHELL = [
   './',
@@ -10,7 +10,7 @@ const APP_SHELL = [
   './icon-maskable.svg',
 ];
 
-const CDN_HOSTS = ['cdn.jsdelivr.net', 'unpkg.com'];
+const CDN_HOSTS = ['cdn.jsdelivr.net', 'unpkg.com', 'esm.run'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
